82ae31d3b1afe3a7caab994cc470d7aeda8d9e2f
José María Márquez Crespo <josemaria.marquez@ust.com>
2024-03-29 12:35:25 +0100

    initial commit: initial project

---
+++ CoffeeShopsApp/CoffeeShopsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeShopsApp
+//
+//  Created by José María Márquez Crespo on 29/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
