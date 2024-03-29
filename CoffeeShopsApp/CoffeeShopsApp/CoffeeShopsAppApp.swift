15fe60dd5cc2ac91cd8d6fcaa9ba98f5871dfde2
José María Márquez Crespo <josemaria.marquez@ust.com>
2024-03-29 12:35:25 +0100

    initial commit: initial project

---
+++ CoffeeShopsApp/CoffeeShopsApp/CoffeeShopsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopsAppApp.swift
+//  CoffeeShopsApp
+//
+//  Created by José María Márquez Crespo on 29/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
