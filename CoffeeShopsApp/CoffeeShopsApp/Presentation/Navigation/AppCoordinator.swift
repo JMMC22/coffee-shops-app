f4a1958f1aa64f5a28e90bab88b90ab071233d9f
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-03-30 14:58:32 +0100

    feat: add app coodinator

---
+++ CoffeeShopsApp/CoffeeShopsApp/Presentation/Navigation/AppCoordinator.swift
@@ -0,0 +1,60 @@
+//
+//  AppCoordinator.swift
+//  CoffeeShopsApp
+//
+//  Created by José María Márquez Crespo on 30/3/24.
+//
+
+import Foundation
+import SwiftUI
+
+class AppCoordinator: ObservableObject {
+    @Published var path = NavigationPath()
+    @Published var presentedItem: AppCoordinator.Page?
+}
+
+extension AppCoordinator {
+    enum Page: Hashable, Identifiable {
+        case home
+        case coffeShopDetails(_ coffeShop: Place)
+        case favouritesCoffeeShops
+
+        var id: String {
+            String(describing: self)
+        }
+    }
+
+    // MARK: - Navigate
+    func push(_ page: AppCoordinator.Page) {
+        path.append(page)
+    }
+
+    func pop() {
+        path.removeLast()
+    }
+
+    func popToRoot() {
+        path.removeLast(path.count)
+    }
+
+    // MARK: - Sheet
+    func present(_ page: AppCoordinator.Page) {
+        presentedItem = page
+    }
+
+    func dismissSheet() {
+        self.presentedItem = nil
+    }
+
+    @ViewBuilder
+    func build(page: AppCoordinator.Page) -> some View {
+        switch page {
+        case .home:
+            Text("HOME")
+        case .coffeShopDetails(let coffeeShop):
+            Text("Details")
+        case .favouritesCoffeeShops:
+            Text("Favourites Coffee Shops View")
+        }
+    }
+}
