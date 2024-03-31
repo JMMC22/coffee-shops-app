782b463af44098b5f3438fc9a448dfcfd4e63e2b
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-03-31 13:31:01 +0200

    feat: add initial details view

---
+++ CoffeeShopsApp/CoffeeShopsApp/Presentation/Screens/Coffee Shop/Details/CoffeeShopDetailsView.swift
@@ -0,0 +1,64 @@
+//
+//  CoffeeShopDetailsView.swift
+//  CoffeeShopsApp
+//
+//  Created by José María Márquez Crespo on 31/3/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct CoffeeShopDetailsView: View {
+
+    @StateObject private var viewModel: CoffeeShopDetailsViewModel
+
+    init(viewModel: CoffeeShopDetailsViewModel) {
+        self._viewModel = StateObject(wrappedValue: viewModel)
+    }
+
+    var body: some View {
+        ScrollView {
+            CoffeeShopDetailsContainerView(viewModel: viewModel)
+        }
+        .navigationTitle(viewModel.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CoffeeShopDetailsContainerView: View {
+
+    @ObservedObject private var viewModel: CoffeeShopDetailsViewModel
+
+    init(viewModel: CoffeeShopDetailsViewModel) {
+        self.viewModel = viewModel
+    }
+
+    var body: some View {
+        VStack {
+            image()
+            information()
+            staticMap()
+        }
+    }
+
+    private func image() -> some View {
+        Rectangle()
+            .fill(.darkGrayText)
+            .frame(height: 350)
+    }
+
+    private func information() -> some View {
+        VStack {
+            Text(viewModel.address)
+                .CSFont(.inter(14, weight: .regular), color: .blackText)
+            Text(viewModel.isOpenNow ? "Open Now" : "Closed")
+                .CSFont(.inter(14, weight: .regular), color: viewModel.isOpenNow ? .greenOpen : .redClosed)
+        }
+    }
+
+    private func staticMap() -> some View {
+        Map(coordinateRegion: $viewModel.coordinate)
+            .disabled(true)
+            .frame(height: 200)
+    }
+}
