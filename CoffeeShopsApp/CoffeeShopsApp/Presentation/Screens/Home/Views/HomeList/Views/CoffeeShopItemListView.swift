9152611207c402697bf19bd5ce05514aa830d2ed
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-03-31 13:11:23 +0200

    feat: add initial design to coffee shops list

--- CoffeeShopsApp/CoffeeShopsApp/Presentation/Screens/Home/Views/HomeList/Views/CoffeeShopItemListView.swift
+++ CoffeeShopsApp/CoffeeShopsApp/Presentation/Screens/Home/Views/HomeList/Views/CoffeeShopItemListView.swift
@@ -15,7 +15,44 @@ struct CoffeeShopItemListView: View {
         self.place = place
     }
 
+    private var statusColor: Color {
+        place.isOpen ? .customGreenOpen : .customRedClosed
+    }
+
     var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                icon()
+                name()
+                Spacer()
+                info()
+            }
+            Divider()
+        }
+    }
+
+    private func icon() -> some View {
+        Image("coffee-cup")
+            .resizable()
+            .frame(width: 18, height: 30)
+            .padding(12)
+            .background(Color.customOliveGreen)
+            .clipShape(Circle())
+    }
+    
+    private func name() -> some View {
         Text(place.name)
+            .CSFont(.inter(14, weight: .medium), color: .blackText)
+    }
+    
+    private func info() -> some View {
+        VStack {
+            Text(place.isOpen ? "Open" : "Closed")
+                .CSFont(.inter(10, weight: .bold), color: .customWhite)
+                .textCase(.uppercase)
+                .padding(6)
+                .background(statusColor)
+                .clipShape(Capsule())
+        }
     }
 }
