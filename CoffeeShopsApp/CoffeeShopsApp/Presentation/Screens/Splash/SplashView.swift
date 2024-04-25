c41f125b2cf1700bc3a7883a1a22c461286de49a
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-04-25 14:14:32 +0200

    feat: add empty splash view

---
+++ CoffeeShopsApp/CoffeeShopsApp/Presentation/Screens/Splash/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  CoffeeShopsApp
+//
+//  Created by José María Márquez Crespo on 25/4/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
