3f648c2ad82823dc8f0a0e5981f1347bd8e0e331
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-05-01 01:58:49 +0200

    feat: add opening hours parameter

---
+++ CoffeeShopsApp/CoffeeShopsApp/Domain/Entities/WeekDay.swift
@@ -0,0 +1,19 @@
+//
+//  WeekDay.swift
+//  CoffeeShopsApp
+//
+//  Created by José María Márquez Crespo on 27/4/24.
+//
+
+import Foundation
+
+enum WeekDay: Int {
+    case monday = 1
+    case tuesday = 2
+    case wednesday = 3
+    case thursday = 4
+    case friday = 5
+    case saturday = 6
+    case sunday = 7
+    case unknown
+}
