8f4f3d54b893c76017f4ee962e89304c4c85c927
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-05-01 22:59:52 +0200

    feat: add weekdays localized strings

--- CoffeeShopsApp/CoffeeShopsApp/Domain/Entities/WeekDay.swift
+++ CoffeeShopsApp/CoffeeShopsApp/Domain/Entities/WeekDay.swift
@@ -8,12 +8,35 @@
 import Foundation
 
 enum WeekDay: Int {
+    case sunday = 0
     case monday = 1
     case tuesday = 2
     case wednesday = 3
     case thursday = 4
     case friday = 5
     case saturday = 6
-    case sunday = 7
     case unknown
+}
+
+extension WeekDay {
+    var loacalized: String {
+        switch self {
+        case .monday:
+            return String(localized: "monday")
+        case .tuesday:
+            return String(localized: "tuesday")
+        case .wednesday:
+            return String(localized: "wednesday")
+        case .thursday:
+            return String(localized: "thursday")
+        case .friday:
+            return String(localized: "friday")
+        case .saturday:
+            return String(localized: "saturday")
+        case .sunday:
+            return String(localized: "sunday")
+        case .unknown:
+            return ""
+        }
+    }
 }
