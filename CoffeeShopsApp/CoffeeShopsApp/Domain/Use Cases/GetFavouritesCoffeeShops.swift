09564f70459346ba21501299f79af47d4be2e7d5
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-06-04 23:25:11 +0200

    feat: add sort places by distance from user

--- CoffeeShopsApp/CoffeeShopsApp/Domain/Use Cases/GetFavouritesCoffeeShops.swift
+++ CoffeeShopsApp/CoffeeShopsApp/Domain/Use Cases/GetFavouritesCoffeeShops.swift
@@ -14,14 +14,36 @@ protocol GetFavouritesCoffeeShops {
 class DefaultGetFavouritesCoffeeShops {
 
     private let googlePlacesRepository: GooglePlacesRepository
+    private let locationManager: LocationManager
 
-    init(googlePlacesRepository: GooglePlacesRepository) {
+    init(googlePlacesRepository: GooglePlacesRepository,
+         locationManager: LocationManager = .shared) {
         self.googlePlacesRepository = googlePlacesRepository
+        self.locationManager = locationManager
     }
 }
 
 extension DefaultGetFavouritesCoffeeShops: GetFavouritesCoffeeShops {
     func execute() -> Result<[Place], RequestError> {
-        return googlePlacesRepository.fetchFavouritesCoffeeShops()
+        let result = googlePlacesRepository.fetchFavouritesCoffeeShops()
+
+        switch result {
+        case .success(let response):
+            let places = response
+            let sortedByUserDistance = sortPlacesByDistance(places)
+            return .success(sortedByUserDistance)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+}
+
+// TODO: Duplicated
+extension DefaultGetFavouritesCoffeeShops {
+    private func sortPlacesByDistance(_ places: [Place]) -> [Place] {
+        return places.sorted(by: {
+            locationManager.getDistance(to: $0.coordinate.latitude, longitude: $0.coordinate.longitude) <
+            locationManager.getDistance(to: $1.coordinate.latitude, longitude: $1.coordinate.longitude)
+        })
     }
 }
