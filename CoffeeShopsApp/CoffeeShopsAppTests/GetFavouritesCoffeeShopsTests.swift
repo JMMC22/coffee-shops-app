8303d786b561695d6173e50f589145b73c27b59e
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-07-31 12:21:18 +0200

    test: add get favourites coffee shop success

--- CoffeeShopsApp/CoffeeShopsAppTests/GetFavouritesCoffeeShopsTests.swift
+++ CoffeeShopsApp/CoffeeShopsAppTests/GetFavouritesCoffeeShopsTests.swift
@@ -10,75 +10,78 @@ import XCTest
 
 class GooglePlacesRepositoryStub: GooglePlacesRepository {
 
-    var shouldReturnError: Bool = false
-    var errorToReturn: RequestError = .unknown
+    var getNearbyPlacesResult: Result<PlacesNearbySearch, RequestError>
+    var getPlaceDetailsResult: Result<Place, RequestError>
+    var fetchFavouritesCoffeeShopsResult: Result<[Place], RequestError>
+    var isFavouriteCoffeeShopResult: Bool
+    var updateFavouriteCoffeShopResult: Result<Bool, RequestError>
+
+    init(getNearbyPlacesResult: Result<PlacesNearbySearch, RequestError> = .failure(.unknown),
+         getPlaceDetailsResult: Result<Place, RequestError> = .failure(.unknown),
+         fetchFavouritesCoffeeShopsResult: Result<[Place], RequestError> = .failure(.unknown),
+         isFavouriteCoffeeShopResult: Bool = false,
+         updateFavouriteCoffeShopResult: Result<Bool, RequestError> = .failure(.unknown)) {
+        self.getNearbyPlacesResult = getNearbyPlacesResult
+        self.getPlaceDetailsResult = getPlaceDetailsResult
+        self.fetchFavouritesCoffeeShopsResult = fetchFavouritesCoffeeShopsResult
+        self.isFavouriteCoffeeShopResult = isFavouriteCoffeeShopResult
+        self.updateFavouriteCoffeShopResult = updateFavouriteCoffeShopResult
+    }
 
     func getNearbyPlaces(location: String, radius: String, keyword: String) async -> Result<PlacesNearbySearch, RequestError> {
-        if shouldReturnError {
-            return .failure(errorToReturn)
-        } else {
-            let places = [
-                Place(id: "1", name: "Coffee Shop A", location: nil, isOpen: true,schedule: [], photos: [],
-                      address: "123 Coffee St",url: URL(string: "https://example.com"),phoneNumber: "123-456-7890"
-                ),
-                Place(id: "2", name: "Coffee Shop B", location: nil, isOpen: false, schedule: [], photos: [],
-                      address: "456 Coffee Ave", url: URL(string: "https://example.com"), phoneNumber: "098-765-4321"
-                )
-            ]
-
-            let searchResult = PlacesNearbySearch(places: places, totalPlaces: places.count)
-            return .success(searchResult)
-        }
+        getNearbyPlacesResult
     }
 
     func getPlaceDetails(id: String) async -> Result<Place, RequestError> {
-        if shouldReturnError {
-            return .failure(errorToReturn)
-        } else {
-            let place = Place(id: id, name: "Detailed Place", location: nil, isOpen: true, schedule: [],
-                photos: [], address: "789 Detailed Rd", url: URL(string: "https://example.com"), phoneNumber: "321-654-9870"
-            )
-
-            return .success(place)
-        }
+        getPlaceDetailsResult
     }
 
     func fetchFavouritesCoffeeShops() -> Result<[Place], RequestError> {
-        if shouldReturnError {
-            return .failure(errorToReturn)
-        } else {
-            let favouriteCoffees = [
-                Place(id: "1", name: "Favourite Coffee Shop", location: nil, isOpen: true, schedule: [],
-                    photos: [], address: "101 Favourite Blvd", url: URL(string: "https://example.com"), phoneNumber: "555-123-4567"
-                )
-            ]
-            return .success(favouriteCoffees)
-        }
+        fetchFavouritesCoffeeShopsResult
     }
 
     func isFavouriteCoffeeShop(id: String) -> Bool {
-        if shouldReturnError {
-            return false
-        } else {
-            return id == "1"
-        }
+        isFavouriteCoffeeShopResult
     }
 
     func updateFavouriteCoffeShop(_ place: Place) -> Result<Bool, RequestError> {
-        if shouldReturnError {
-            return .failure(errorToReturn)
-        } else {
-            return .success(true)
-        }
+        updateFavouriteCoffeShopResult
     }
 }
 
 final class GetFavouritesCoffeeShopsTests: XCTestCase {
-    
-    func test_execute_success_return_sorted_array_when_repository_return_nonEmpty_array() {
-        
+
+    func test_execute_success_return_sorted_array_when_repository_return_nonEmpty_array() throws {
+
         // GIVEN
-        let stub = GooglePlacesRepositoryStub()
+        let mockArray = [
+            Place(id: "1",name: "Coffee Shop A", location: PlaceLocation(latitude: 40.7128, longitude: -74.0060), // New York City
+                isOpen: true, schedule: [], photos: [], address: "123 Coffee St, New York, NY",
+                url: URL(string: "https://example.com/place1"), phoneNumber: "123-456-7890"
+            ),
+            Place(
+                id: "2", name: "Coffee Shop B", location: PlaceLocation(latitude: 34.0522, longitude: -118.2437), // Los Angeles
+                isOpen: false, schedule: [], photos: [], address: "456 Coffee Ave, Los Angeles, CA",
+                url: URL(string: "https://example.com/place2"), phoneNumber: "098-765-4321"
+            ),
+            Place(id: "3", name: "Coffee Shop C", location: PlaceLocation(latitude: 51.5074, longitude: -0.1278), // London
+                isOpen: true, schedule: [], photos: [], address: "789 Coffee Road, London, UK",
+                url: URL(string: "https://example.com/place3"), phoneNumber: "112-233-4455"
+            )
+        ]
+
+        let result:  Result<[Place], RequestError> = .success(mockArray)
+        let stub = GooglePlacesRepositoryStub(fetchFavouritesCoffeeShopsResult: result)
         let sut = DefaultGetFavouritesCoffeeShops(googlePlacesRepository: stub)
+
+        // WHEN
+        let capturedResult = sut.execute()
+
+        // THEN
+        let capturedCoffeeShopsList = try XCTUnwrap(capturedResult.get())
+
+        XCTAssertEqual(capturedCoffeeShopsList[0], mockArray[2])
+        XCTAssertEqual(capturedCoffeeShopsList[1], mockArray[0])
+        XCTAssertEqual(capturedCoffeeShopsList[2], mockArray[1])
     }
 }
